85f66bbcaa6e135934baf4dc94caf9980fd688e9
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-06-25 23:56:06 -0300

    feat: enable logging in production environment for Sinatra application

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@
 
 require 'sinatra'
 
+configure :production do
+  # Ativa o logging interno do Sinatra somente no ambiente de produção
+  # Isso faz com que mensagens de requisição, erros e avisos sejam exibidas no console
+  # Serviços de hospedagem como Render capturam esses logs e exibem no dashboard
+  enable :logging
+end
+
 get '/' do
-    @title = 'fernandojose.dev'
-    erb :index, layout: :page
+  @title = 'fernandojose.dev'
+  erb :index, layout: :page
 end
