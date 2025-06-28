245255cd70a1b8285fcaab4babcf9c72cc7da2a9
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-06-27 23:07:33 -0300

    chore: add frozen_string_literal comment and update ENV fetch defaults in puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
-port ENV.fetch('PORT') { 9292 }
-environment ENV.fetch('RACK_ENV') { 'production' }
+# frozen_string_literal: true
+
+port ENV.fetch('PORT', 9292)
+environment ENV.fetch('RACK_ENV', 'production')
 
 workers 0
 threads 1, 6
