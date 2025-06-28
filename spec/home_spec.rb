8ab893cafad6135766c4b1441ff9790d95f4cc16
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-06-28 11:28:28 -0300

    test: add RSpec tests for the home page and create home view template

---
+++ spec/home_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rack/test'
+require 'rspec'
+require 'nokogiri'
+require_relative '../app'
+
+RSpec.describe 'Página HOME(principal)' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def doc(response)
+    Nokogiri::HTML(response.body)
+  end
+
+  it 'retorna com sucesso e contém um h1 e o título correto' do
+    get '/'
+
+    expect(last_response).to be_ok
+
+    # Verifica se tem um <h1>
+    expect(doc(last_response).at_css('h1')).not_to be_nil
+
+    # Verifica o conteúdo da <title>
+    title = doc(last_response).at_css('title').content
+    expect(title).to eq('fernandojose.dev')
+  end
+end
