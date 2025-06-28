8d0eb675c3f06bb329b0d4e6107996ee34c6c02e
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-06-27 23:01:15 -0300

    refactor: remove production logging configuration from app.rb

--- app.rb
+++ app.rb
@@ -2,13 +2,6 @@
 
 require 'sinatra'
 
-configure :production do
-  # Ativa o logging interno do Sinatra somente no ambiente de produção
-  # Isso faz com que mensagens de requisição, erros e avisos sejam exibidas no console
-  # Serviços de hospedagem como Render capturam esses logs e exibem no dashboard
-  enable :logging
-end
-
 get '/' do
   @title = 'fernandojose.dev'
   erb :index, layout: :page
